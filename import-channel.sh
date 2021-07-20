57dee77d98e93d37020bf7b148b529644d9d45b8
Preston TW <preston@commure.com>
2021-07-20 11:46:22 -0400

    Use `--data-binary` instead of `-d`
    leads to preserving newlines! Yay!

--- import-channel.sh
+++ import-channel.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -s -u admin:admin -k -X POST "https://localhost:8443/api/channels" -H  "accept: application/json" -H  "Content-Type: application/xml" -d @./saved-channel.xml | jq -e .boolean
+curl -s -u admin:admin -k -X POST "https://localhost:8443/api/channels" -H  "accept: application/json" -H  "Content-Type: application/xml" --data-binary @./saved-channel.xml | jq -e .boolean
